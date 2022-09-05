c4f30e883a84372cc908dce48db33bae61722e0f
moon0706 <hajung7424@naver.com>
2022-09-05 21:06:50 +0900

    add count.cc

---
+++ count.cc
@@ -0,0 +1,11 @@
+#include "count.hh"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+void count_(int num){
+	for (int i=1; i<=num;i++)
+		cout<< i << endl;
+	cout << "Start!!" <<endl;
+	cout << "201931747 Hajung Moon" ;
+}
